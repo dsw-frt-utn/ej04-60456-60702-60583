f84f6b369291067409996fd1e553b1cf1bb8bc6a
Sebastian Mieres <sebassegundomieres@gmail.com>
2026-04-20 23:25:38 -0300

    Agrega ventana de menu principal

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -1,13 +1,13 @@
 package app;
 
 import data.Persistencia;
-import views.MainView; 
+import views.MainView; // Importamos el menú principal
 
 public class Program {
     public static void main(String[] args) {
         Persistencia.inicializar();
         
-        
+        // Arrancamos la aplicación desde el Menú Principal
         MainView view = new MainView();
         view.setVisible(true);
     }
